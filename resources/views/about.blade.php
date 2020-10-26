a539cc4c5f0ca0069ce9c08643c1248835ebcad1
sanhidalgoo <shidalgoo1@eafit.edu.co>
2020-10-25 23:13:03 -0500

    About information

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,8 +1,12 @@
 @extends('layout')
 
 @section('title', 'about')
-    
+
 
 @section('content')
-    <h1>About</h1>
+    <div class="div container">
+        <h1 class="display-4">About</h1>
+        <p class="lead text-secondary">We are a company that publishes information about cars. You can see our catalog and
+            contact us.</p>
+    </div>
 @endsection
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,26 +4,39 @@
 
 
 @section('content')
-    <h1 class="display-4">Contact</h1>
+
     <div class="container">
-        <form class="bg-white shadow rounded py-3 px-4" method="POST" action="">
+        <form class="bg-white shadow rounded py-3 px-4" method="GET" action="{{ route('home') }}">
+            <h1 class="display-4">Contact</h1>
+            <hr>
             @csrf
             <div class="form-group">
                 <label for="name">Name</label>
-                <input class="form-control bg-light shadow-sm border-0" id="name" name="name" placeholder="Nombre..." value="{{ old('name') }}">
+                <input class="form-control bg-light shadow-sm border-0" id="name" name="name" placeholder="Nombre..."
+                    value="{{ old('name') }}">
                 {!! $errors->first('name', '<small>:message</small><br>') !!}
             </div>
 
-            <input type="text" name="email" placeholder="Email..." value="{{ old('email') }}"> <br>
-            {!! $errors->first('email', '<small>:message</small><br>') !!}
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input class="form-control bg-light shadow-sm border-0" id="email" type="text" name="email"
+                    placeholder="Email..." value="{{ old('email') }}"> <br>
+                {!! $errors->first('email', '<small>:message</small><br>') !!}
+            </div>
 
-            <input name="subject" placeholder="Asunto..." value="{{ old('subject') }}"> <br>
-            {!! $errors->first('subject', '<small>:message</small><br>') !!}
+            <div class="fomer-group">
+                <label for="subject">Subject</label>
+                <input class="form-control bg-light shadow-sm border-0" id="subject" name="subject" placeholder="Subject..."
+                    value="{{ old('subject') }}"> <br>
+                {!! $errors->first('subject', '<small>:message</small><br>') !!}
+            </div>
 
-            <textarea name="content" placeholder="Mensaje">{{ old('content') }}</textarea> <br>
-            {!! $errors->first('content', '<small>:message</small><br>') !!}
-
-            <button class="btn btn-primary btn-block">Send</button>
+            <div class="former-group">
+                <label for="message">Messae</label>
+                <textarea class="form-control bg-light shadow-sm border-0" id="message" name="content" placeholder="Message">{{ old('content') }}</textarea> <br>
+                {!! $errors->first('content', '<small>:message</small><br>') !!}
+            </div>
+            {{-- <button class="btn btn-primary btn-block">Send</button> --}}
         </form>
     </div>
 @endsection
